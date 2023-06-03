9d5e7428613fa98ce6cb53e4a9a1c98500bdf01f
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2023-06-03 01:19:53 -0600

    fixed login url

--- components/EmailLogin/index.jsx
+++ components/EmailLogin/index.jsx
@@ -21,7 +21,7 @@ const auth = getAuth(app);
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: "http://localhost:3000/email-login",
+  url: "https://scavng.com/email-login",
   // This must be true.
   handleCodeInApp: true,
 };
