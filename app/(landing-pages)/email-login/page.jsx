8305c530b328b7f5d9229e2ebdf843553159ae67
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2023-06-03 01:33:52 -0600

    fix?

--- app/(landing-pages)/email-login/page.jsx
+++ app/(landing-pages)/email-login/page.jsx
@@ -8,73 +8,76 @@ import Link from "next/link";
 
 import { app } from "../../../components/initializeFirebase";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const auth = getAuth(app);
 
 const EmailSigninPage = () => {
   const router = useRouter();
 
-  if (isSignInWithEmailLink(auth, window.location.href)) {
-    // Additional state parameters can also be passed via URL.
-    // This can be used to continue the user's intended action before triggering
-    // the sign-in operation.
-    // Get the email if available. This should be available if the user completes
-    // the flow on the same device where they started it.
-    let email = window.localStorage.getItem("emailForSignIn");
-    if (!email) {
-      // User opened the link on a different device. To prevent session fixation
-      // attacks, ask the user to provide the associated email again. For example:
-      email = window.prompt("Please provide your email for confirmation");
+  useEffect(() => {
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      // Additional state parameters can also be passed via URL.
+      // This can be used to continue the user's intended action before triggering
+      // the sign-in operation.
+      // Get the email if available. This should be available if the user completes
+      // the flow on the same device where they started it.
+      let email = window.localStorage.getItem("emailForSignIn");
+      if (!email) {
+        // User opened the link on a different device. To prevent session fixation
+        // attacks, ask the user to provide the associated email again. For example:
+        email = window.prompt("Please provide your email for confirmation");
+      }
+      // The client SDK will parse the code from the link for you.
+      signInWithEmailLink(auth, email, window.location.href)
+        .then(async (result) => {
+          // Clear email from storage.
+          window.localStorage.removeItem("emailForSignIn");
+
+          console.log(result);
+          const user = result.user;
+          console.log(user);
+          // You can access the new user via result.user
+          // Additional user info profile not available via:
+          // result.additionalUserInfo.profile == null
+          // You can check if the user is new or existing:
+          // result.additionalUserInfo.isNewUser
+
+          // check if firestore user
+          await fetch("/api/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              uid: user.uid,
+              displayname: user.displayName,
+              email: user.email,
+              photo: user.photoURL,
+            }),
+          });
+
+          // fetch to /api/stripecreatecustomer
+          await fetch("/api/stripecreatecustomer", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: user.email,
+              name: user.displayName,
+              user_id: user.uid,
+            }),
+          });
+
+          router.push("/sheets");
+        })
+        .catch((error) => {
+          // Some error occurred, you can inspect the code: error.code
+          // Common errors could be invalid email and invalid or expired OTPs.
+        });
     }
-    // The client SDK will parse the code from the link for you.
-    signInWithEmailLink(auth, email, window.location.href)
-      .then(async (result) => {
-        // Clear email from storage.
-        window.localStorage.removeItem("emailForSignIn");
-
-        console.log(result);
-        const user = result.user;
-        console.log(user);
-        // You can access the new user via result.user
-        // Additional user info profile not available via:
-        // result.additionalUserInfo.profile == null
-        // You can check if the user is new or existing:
-        // result.additionalUserInfo.isNewUser
-
-        // check if firestore user
-        await fetch("/api/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            uid: user.uid,
-            displayname: user.displayName,
-            email: user.email,
-            photo: user.photoURL,
-          }),
-        });
-
-        // fetch to /api/stripecreatecustomer
-        await fetch("/api/stripecreatecustomer", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: user.email,
-            name: user.displayName,
-            user_id: user.uid,
-          }),
-        });
-
-        router.push("/sheets");
-      })
-      .catch((error) => {
-        // Some error occurred, you can inspect the code: error.code
-        // Common errors could be invalid email and invalid or expired OTPs.
-      });
-  }
+  }, []);
 
   return (
     <>
