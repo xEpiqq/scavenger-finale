600b99403ae6d0d12e8d7f5a69424b1bc479b2be
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2023-09-12 18:38:35 -0600

    sorted the lists

--- app/api/create-email/route.js
+++ app/api/create-email/route.js
@@ -1,71 +1,26 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-import uuid from "uuid";
-
-// Create a new configuration object with your API key\
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_KEY,
-  baseUrl: "https://api.openai.com/v1/chat/completions/",
-});
-const openai = new OpenAIApi(configuration);
-
 import { app } from "../../../components/initializeFirebase";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 import ListItem from "../../(app-pages)/sheets/(sheet)/ListItem.js"
 const db = getFirestore(app);
 
+const function_url = "https://createemail-pui7kvv4mq-uc.a.run.app"
+
 export async function POST(request) {
   const body = await request.json();
-  const { item } = body;
 
-  if (!item) {
-    return new Response(JSON.stringify({ error: "No item" }));
-  }
+  const google_function_response = await fetch (function_url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        body
+        })
+    })
+    // google_function_response.body is a readable stream, so we need to convert it to JSON
+    const response_data = await google_function_response.json()
+    console.log(response_data)
 
-  // TODO: we need to verify that the user has a subscription
-
-  try {
-    const model = "gpt-3.5-turbo";
-
-    const prompt =
-      "You will write just the body(no subject) of a cold outreach email for a local buisness from a freelance web developer. Max 700 characters" +
-      "Here is some data about the local buisness:\n" +
-      "Name: " +
-      item.name +
-      "Website: " +
-      item.website +
-      "Email: " +
-      item.email +
-      "Phone: " +
-      item.phone +
-      "Address: " +
-      item.address +
-      "Here is some data about the freelance web developer" +
-      "Name: " +
-      "Scavenger";
-
-    const completion = await openai.createChatCompletion({
-      model: model,
-      messages: [{ role: "system", content: prompt }],
-      temperature: 0.9,
-    });
-    console.log(completion.data);
-
-    if (completion.data.choices[0].text == "undefined") {
-      return new Response(JSON.stringify({ data: "undefined" }));
-    }
-    item.emailBody = completion.data.choices[0].message.content
-    ListItem.updateItem(item)
-    return new Response(
-      JSON.stringify({ data: completion.data.choices[0].message.content })
-    );
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
-    }
-  }
+  NextResponse.json({ success: "Function Fired" }, { status: 200 })
 }
